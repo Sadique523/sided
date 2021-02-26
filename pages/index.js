faaf36ceb30e9cb38e8cc16327398d9decc0aa24
Sadique523 <sadik.galaria@gmail.com>
2021-02-26 18:53:42 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+
+const API = 'https://newsapi.org/v2/top-headlines';
+const SOURCE_API = 'https://newsapi.org/v2/sources'
+const API_KEY = 'ff4f1ab5e1fb4775ae6ca88a6f13a72b'
 
 export default function Home() {
+
+  const [data, setData] = React.useState([]);
+  const [source, setSource] = React.useState('the-times-of-india')
+
+  React.useEffect(() => {
+    fetch(`${SOURCE_API}?country=in&apiKey=${API_KEY}`)
+    .then(res => res.json())
+    .then(res => console.log(res));
+    fetch(`${API}?sources=${source}&apiKey=${API_KEY}`)
+    .then(res => res.json())
+    .then(res => setData(res.articles));
+  }, [source]);
+
+  const getNews = () => {
+    return data.map(({title, urlToImage, url}) => (
+      <li className="w-3/12 laptop:w-4/12 tablet:w-6/12 mobile:w-full">
+        <div className="shadow rounded-sm m-4">
+          <img width="100%" src={urlToImage}/>
+          <div className="p-4">
+            <span>{title}</span>
+          </div>
+        </div>  
+      </li>
+    ))
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className="p-4 font-Reggae">
+      <select style={{outline: 'none'}} onChange={(e) => setSource(e.target.value)}>
+        <option value="the-times-of-india">Left</option>
+        <option value="google-news-in">Neutral</option>
+        <option value="the-hindu">Right</option>
+      </select>
+      <span className="m-4 text-5xl">Sided</span>
+      <ul className="flex flex-wrap">
+        {getNews()}
+      </ul>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'mobile': '100px',
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        'Mukta': ['Mukta'],
+        'Reggae': ['Reggae One']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
